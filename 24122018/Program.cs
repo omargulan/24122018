db8a57fd164fd3c568d879cff5a342bdfa420dc0
Margulan Omirzakov <margulan89@gmail.com>
2019-01-20 23:14:21 +0600

    first commit

---
+++ 24122018/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _24122018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
